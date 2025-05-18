a693c57699f6cb87efbe942eb9aaa613e4705428
Luke Arpino <luke.c.arpino@gmail.com>
2025-05-18 17:48:10 +0100

    Add: initial clojure setup
    * Can't get bazel to work nicely with clojure, so using lein

---
+++ cljlox/lox/core.clj
@@ -0,0 +1,28 @@
+(ns lox.core
+  (:gen-class))
+
+(defn- run [source]
+  source)
+
+(defn- run-file [& args])
+
+(defn- run-prompt []
+  (loop []
+    (print "> ")
+    (flush)
+    (when-some [line (read-line)]
+      (println (run line))
+      (recur))))
+
+(defn -main [& args]
+  (try
+    (let [arglen (count args)]
+      (cond (> arglen 1)
+            (println "Usage: cljlox [script]")
+            (= arglen 1)
+            (run-file (first args))
+            :else
+            (run-prompt)))
+    (catch Exception e
+      (println  (format "Fatal error: %s" (ex-message e))))))
+
