92c40afcf280b8b37e6152857b2600621b2babeb
Luke Arpino <luke.c.arpino@gmail.com>
2025-06-02 14:26:25 +0100

    Add: chapter 7 (Java)

--- jlox/com/craftinginterpreters/lox/Lox.java
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +30,9 @@ public class Lox {
 
         if (hadError) {
             System.exit(65);
+        }
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -57,9 +62,7 @@ public class Lox {
             return;
         }
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -77,5 +80,10 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
