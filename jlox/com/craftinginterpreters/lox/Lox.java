a48c21fc7778158320b1fb0fb33267ac21a875bf
Luke Arpino <luke.c.arpino@gmail.com>
2025-06-02 13:43:57 +0100

    Add: chapter 6 (Java)

--- jlox/com/craftinginterpreters/lox/Lox.java
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -49,6 +49,13 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error
+        if (hadError) {
+            return;
+        }
 
         for (Token token : tokens) {
             System.out.println(token);
@@ -62,5 +69,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end ", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
