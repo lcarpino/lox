a840381ed0e97c72a303a082a2c82afca79fbb7a
Luke Arpino <luke.c.arpino@gmail.com>
2026-07-16 19:07:43 +0100

    Fix: user.clj file

--- cljlox/dev/user.clj
+++ cljlox/dev/user.clj
@@ -3,7 +3,7 @@
             [malli.dev.pretty]))
 
 (defn dev-mode-on! []
-  (malli.dev/start! {:report (malli.dev.pretty/thrower)})
+  (malli.dev/start! {:report (malli.dev.pretty/thrower)}))
 
 (defn dev-mode-off! []
-  (malli.dev/stop!)
+  (malli.dev/stop!))
