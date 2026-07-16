7ba27c1165f9c7b628dc0e7d7fbf9b4532383452
Luke Arpino <luke.c.arpino@gmail.com>
2026-07-16 19:35:52 +0100

    Refactor: zprint user.clj

--- cljlox/dev/user.clj
+++ cljlox/dev/user.clj
@@ -2,8 +2,6 @@
   (:require [malli.dev]
             [malli.dev.pretty]))
 
-(defn dev-mode-on! []
-  (malli.dev/start! {:report (malli.dev.pretty/thrower)}))
+(defn dev-mode-on! [] (malli.dev/start! {:report (malli.dev.pretty/thrower)}))
 
-(defn dev-mode-off! []
-  (malli.dev/stop!))
+(defn dev-mode-off! [] (malli.dev/stop!))
