7cb47dc149da9d07337a03a24049ca09481ee357
Luke Arpino <luke.c.arpino@gmail.com>
2026-07-26 15:51:00 +0100

    Add: exec script for cljlox

---
+++ scripts/cljlox.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+PROJECT_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)/.."
+JAR_PATH="$PROJECT_DIR/target/uberjar/cljlox-0.1.0-SNAPSHOT-standalone.jar"
+
+if [ ! -f "$JAR_PATH" ]; then
+    echo "Error: $JAR_PATH not found." >&2
+    echo "Please run 'lein uberjar' in $PROJECT_DIR first." >&2
+    exit 1
+fi
+
+java -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -jar "$JAR_PATH" "$@"
