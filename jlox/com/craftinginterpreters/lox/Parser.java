8f321dc1102b078f3ba593993fc1420bca7654f5
Luke Arpino <luke.c.arpino@gmail.com>
2026-07-26 08:05:09 +0100

    Fix: jlox passes test suite

--- jlox/com/craftinginterpreters/lox/Parser.java
+++ jlox/com/craftinginterpreters/lox/Parser.java
@@ -198,7 +198,7 @@ class Parser {
         if (!check(RIGHT_PAREN)) {
             do {
                 if (parameters.size() >=255) {
-                    error(peek(), "Can't have more than 255 parameters");
+                    error(peek(), "Can't have more than 255 parameters.");
                 }
 
                 parameters.add(consume(IDENTIFIER, "Expect parameter name."));
