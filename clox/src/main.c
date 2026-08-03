3a2b992d7eb973c970d050aa43ba14eedbf59aec
Luke Arpino <luke.c.arpino@gmail.com>
2026-08-03 07:14:53 +0100

    Add: clox chunk structure

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,5 +1,13 @@
+#include "clox/chunk.h"
 #include "clox/common.h"
+#include "clox/debug.h"
 
 int main(int argc, const char* argv[]) {
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
   return 0;
 }
