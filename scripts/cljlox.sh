126732d31aff54160ee35533b9d40dbfd407837d
Luke Arpino <luke.c.arpino@gmail.com>
2026-08-04 13:24:55 +0100

    Fix: wrapper script

--- scripts/cljlox.sh
+++ scripts/cljlox.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
 PROJECT_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)/.."
-JAR_PATH="$PROJECT_DIR/target/uberjar/cljlox-0.1.0-SNAPSHOT-standalone.jar"
+JAR_PATH="$PROJECT_DIR/target/cljlox.jar"
 
 if [ ! -f "$JAR_PATH" ]; then
     echo "Error: $JAR_PATH not found." >&2
-    echo "Please run 'lein uberjar' in $PROJECT_DIR first." >&2
+    echo "Please run 'clojure -T:build uber' in $PROJECT_DIR first." >&2
     exit 1
 fi
 
